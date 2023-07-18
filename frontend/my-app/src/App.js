debe5dbc955c071fe9aaee5dfdd2ef997827ea7d
Paula Quadros <paula.mendonca@icomp.ufam.edu.br>
2023-07-18 13:32:58 -0400

    Card and props added

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,172 +1,203 @@
 import React, { Component } from "react";
 
-const Header = () => {
-  return (
-    <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <ul className="nav nav-pills">
-        <li>
-          <h2>Store©</h2>
-        </li>
-        <li className="nav-item dropdown" id="produtos">
-          <button
-            className="btn btn-dark dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Products
-          </button>
-          <ul className="dropdown-menu dropdown-menu-dark">
-            <li>
-              <a className="dropdown-item" href="/create">
-                Create
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="/update">
-                Update
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="/delete">
-                Delete
-              </a>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item" id="login">
-          <a href="google.com" className="nav-link px-2 link-dark">
-            Login
-          </a>
-        </li>
-        <li className="nav-item" id="signup">
-          <a href="google.com" className="nav-link px-2 link-dark">
-            Sign Up
-          </a>
-        </li>
-        <li>
-          <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" id="search">
-            <input
-              type="search"
-              className="form-control"
-              placeholder="Search..."
-              aria-label="Search"
-            />
-          </form>
-        </li>
-      </ul>
-    </header>
-  );
-};
-
-const Body = () => {
+const Body = (props) => {
   return (
     <div className="container">
-      <div className="card-group">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">PS5</h5>
-            <p className="card-text">
-              Largura de banda da RAM: 448 GB/s; Taxa de transferência da RAM:
-              14 GB/s; Armazenamento interno: SSD customizado de 825 GB,
-              barramento de 12 canais e controlador PCI Express 4.0, com taxa de
-              transferência de 5,5 GB/s (tamanho original) ou 8,9 GB/s
-              (compactado);
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">R$ 4.999,90</small>
-            </p>
+      <div className="row">
+        {props.products.map((product, index) => (
+          <div className="col-lg-4 col-md-6 mb-4" key={index}>
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.description}</p>
+                <p className="card-text">
+                  <small className="text-body-primary">{product.price}</small>
+                </p>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => props.addToCart(product)}
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Amazon Echo Dot</h5>
-            <p className="card-text">
-              O Echo Dot é o smart speaker de maior sucesso. Controlado por voz
-              com Alexa, ele é perfeito para qualquer ambiente. Você pode pedir
-              músicas, notícias, informações e muito mais. Além de ligar para
-              amigos e familiares e controlar dispositivos compatíveis de casa
-              inteligente com sua voz.
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">R$ 349,00</small>
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Xbox Series X</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">R$ 4.299.00</small>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="card-group">
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Novo Kindle 11ª Geração</h5>
-            <p className="card-text">
-              O Kindle mais leve e compacto, agora com tela de 300 ppi de alta
-              resolução para textos e imagens nítidos. Leia confortavelmente em
-              uma tela antirreflexo como se fosse em papel. Com a iluminação
-              embutida ajustável e o modo noturno, ler fica mais fácil durante o
-              dia e a noite.
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">R$ 399,00</small>
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">
-              BLACK+DECKER Aspirador 3 em 1 Vertical
-            </h5>
-            <p className="card-text">
-              Aspirador vertical Muito leve e comprido,é perfeito para aspiraros
-              lugares díficies como embaixo de sofás e camas. Filtro HEPA
-              permanente Garante maior filtragemdo ar, além de tornar
-              suasfaxinas mais sustentáveis.È só lavar e usar de novo
-              Reservatório de 500ml Faça a limpeza sem sepreocupar em esvaziar
-              oreservatório várias vezes! Lanterna frontal Ilumine e aspire
-              assujeirinhas mais difícies deenxergar Aspirador portátil.
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">R$ 639,90</small>
-            </p>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">Apple iPhone 14 (128 GB)</h5>
-            <p className="card-text">
-              Tela Super Retina XDR de 6,1 polegadas Sistema de câmera avançado
-              para fotos melhores em qualquer luz Modo Cinema, agora em 4K Dolby
-              Vision até 30 qps Modo Ação para vídeos em movimento com mais
-              estabilidade Tecnologia de segurança — Detecção de Acidente, que
-              liga para a emergência se você não puder.
-            </p>
-            <p className="card-text">
-              <small className="text-body-secondary">R$ 4.999,00</small>
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
 };
 
+const Cart = (props) => {
+  return (
+    <div className="container">
+      <h2>Seu carrinho</h2>
+
+      {props.cart.length === 0 ? (
+        <p>Seu carrinho está vazio</p>
+      ) : (
+        props.cart.map((product, index) => (
+          <div
+            key={index}
+            style={{
+              borderTop: "1px solid #ccc",
+              padding: "30px",
+            }}
+          >
+            <h5>{product.name}</h5>
+            <p>{product.price}</p>
+            <button
+              className="btn btn-danger"
+              onClick={() => props.removeFromCart(index)}
+            >
+              Remover do carrinho
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
 class App extends Component {
+  state = {
+    cart: [],
+    page: "products",
+  };
+
+  addToCart = (product) => {
+    this.setState((prevState) => ({
+      cart: [...prevState.cart, product],
+    }));
+  };
+
+  removeFromCart = (index) => {
+    this.setState((prevState) => ({
+      cart: prevState.cart.filter((_, i) => i !== index),
+    }));
+  };
+
+  navigateTo = (page) => {
+    this.setState({ page });
+  };
+
   render() {
+    const products = [
+      {
+        name: "PS5",
+        description:
+          "Largura de banda da RAM: 448 GB/s; Taxa de transferência da RAM: 14 GB/s; Armazenamento interno: SSD customizado de 825 GB, barramento de 12 canais e controlador PCI Express 4.0, com taxa de transferência de 5,5 GB/s (tamanho original) ou 8,9 GB/s (compactado);",
+        price: "R$ 4.999,90",
+      },
+      {
+        name: "Amazon Echo Dot",
+        description:
+          "O Echo Dot é o smart speaker de maior sucesso. Controlado por voz com Alexa, ele é perfeito para qualquer ambiente. Você pode pedir músicas, notícias, informações e muito mais. Além de ligar para amigos e familiares e controlar dispositivos compatíveis de casa inteligente com sua voz.",
+        price: "R$ 349,00",
+      },
+      {
+        name: "PS5",
+        description:
+          "Largura de banda da RAM: 448 GB/s; Taxa de transferência da RAM: 14 GB/s; Armazenamento interno: SSD customizado de 825 GB, barramento de 12 canais e controlador PCI Express 4.0, com taxa de transferência de 5,5 GB/s (tamanho original) ou 8,9 GB/s (compactado);",
+        price: "R$ 4.999,90",
+      },
+      {
+        name: "Xbox Series X",
+        description:
+          "Largura de banda da RAM: 448 GB/s; Taxa de transferência da RAM: 14 GB/s; Armazenamento interno: SSD customizado de 825 GB, barramento de 12 canais e controlador PCI Express 4.0, com taxa de transferência de 5,5 GB/s (tamanho original) ou 8,9 GB/s (compactado);",
+        price: "R$ 4.999,90",
+      },
+      {
+        name: "Amazon Echo Dot",
+        description:
+          "O Echo Dot é o smart speaker de maior sucesso. Controlado por voz com Alexa, ele é perfeito para qualquer ambiente. Você pode pedir músicas, notícias, informações e muito mais. Além de ligar para amigos e familiares e controlar dispositivos compatíveis de casa inteligente com sua voz.",
+        price: "R$ 349,00",
+      },
+      {
+        name: "Xbox Series X",
+        description:
+          "Largura de banda da RAM: 448 GB/s; Taxa de transferência da RAM: 14 GB/s; Armazenamento interno: SSD customizado de 825 GB, barramento de 12 canais e controlador PCI Express 4.0, com taxa de transferência de 5,5 GB/s (tamanho original) ou 8,9 GB/s (compactado);",
+        price: "R$ 4.999,90",
+      },
+    ];
+
     return (
       <div className="container text-left">
-        <Header />
-        <Body />
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <ul className="nav nav-pills">
+            <li>
+              <h2
+                className="store-title"
+                onClick={() => {
+                  this.navigateTo("products");
+                }}
+              >
+                Store
+              </h2>
+            </li>
+            <li className="nav-item dropdown" id="produtos">
+              <button
+                className="btn btn-dark dropdown-toggle"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Products
+              </button>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li>
+                  <a className="dropdown-item" href="/create">
+                    Create
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/update">
+                    Update
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/delete">
+                    Delete
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item" id="login">
+              <a href="google.com" className="nav-link px-2 link-dark">
+                Login
+              </a>
+            </li>
+            <li className="nav-item" id="signup">
+              <a href="google.com" className="nav-link px-2 link-dark">
+                Sign Up
+              </a>
+            </li>
+            <li>
+              <form
+                className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
+                id="search"
+              >
+                <input
+                  type="search"
+                  className="form-control"
+                  placeholder="Search..."
+                  aria-label="Search"
+                />
+              </form>
+            </li>
+            <li
+              className="nav-item link-dark cart-button"
+              id="cart"
+              onClick={() => this.setState({ page: "cart" })}
+            >
+              Cart ({this.state.cart.length})
+            </li>
+          </ul>
+        </header>
+        {this.state.page === "products" ? (
+          <Body products={products} addToCart={this.addToCart} />
+        ) : (
+          <Cart cart={this.state.cart} removeFromCart={this.removeFromCart} />
+        )}
       </div>
     );
   }
